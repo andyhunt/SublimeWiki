86bfeb22933c121342c7bee6b4415a2fa75fa19f
Andy Hunt <andy@toolshed.com>
2016-12-19 15:31:04 -0500

    First version

--- Wiki.py
+++ Wiki.py
@@ -1,7 +1,20 @@
 import sublime, sublime_plugin, os, re
 
+
+def findInSubdirectory(filename, subdirectory=''):
+    if subdirectory:
+        path = subdirectory
+    else:
+        path = os.getcwd()
+    for root, dirs, names in os.walk(path):
+        if filename in names:
+            return os.path.join(root, filename)
+    return ""
+
+
 class WikiLinkCommand(sublime_plugin.TextCommand):
-    def run(self, edit):        
+
+    def run(self, edit):  
         #find our current directory
         directory = os.path.split(self.view.file_name())[0]
         #find our current window
@@ -22,27 +35,29 @@ class WikiLinkCommand(sublime_plugin.TextCommand):
         elif "link.internal.Wiki" in self.view.scope_name(location.a):
             #okay, we're good. Keep on keepin' on.        
             
-            #compile the full file name and path.
-            new_file = os.path.join(directory,word+".wiki")
-
             #debug section: uncomment to write to the console
             # print("Location: %d" % location.a)
             # print("Selected word is '%s'" % word)
             # print("Full file path: %s" % new_file)
             # print("Selected word scope is '%s'" % self.view.scope_name(location.a))
             # if internalLink in self.view.scope_name(location.a):
-            #     print("this is an internal link")
+            #  print("this is an internal link")
             #end debug section
 
-            if os.path.exists(new_file):
+            sublime.active_window().run_command("save")
+
+            found_file = findInSubdirectory(word, directory)
+
+            if os.path.exists(found_file):
                 #open the already-created page.
-                new_view = window.open_file(new_file)
+                new_view = window.open_file(found_file)
             else:
                 #Create a new file and slap in the default text.
                 new_view = window.new_file()
-                default_text = "{0}\nWrite about {0} here.".format(word)
+                default_text = "{0}\n".format(word)
                 new_view.run_command('append', {'characters': default_text})
-                new_view.set_name("%s.wiki" % word)
+                new_view.set_name(word)
                 new_view.set_syntax_file("Packages/Wiki/Wiki.tmLanguage")
         else:
             sublime.status_message("Can only open WikiWords, email addresses or web addresses.")
+
